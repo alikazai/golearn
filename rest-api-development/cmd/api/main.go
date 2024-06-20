0b6068c9af9f0a3125c6f668422fd05be6eaf0f3
Ali Kazai <ali@alikazai.com>
2024-06-20 03:05:25 +0100

    update docker and db connection

--- rest-api-development/cmd/api/main.go
+++ rest-api-development/cmd/api/main.go
@@ -1,11 +1,36 @@
 package main
 
-import "log"
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+)
 
 const (
 	postgresDNS = "postgres://root:secret@localhost:5455/blog?sslmode=disable"
+	driver      = "postgres"
 )
 
 func main() {
+	_, err := connectToDB(driver)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	log.Println("Hello World!")
+}
+
+func connectToDB(driver string) (*sql.DB, error) {
+	db, err := sql.Open(driver, postgresDNS)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- rest-api-development/go.mod
+++ rest-api-development/go.mod
@@ -2,4 +2,7 @@ module github.com/alikazai/golearn
 
 go 1.22.3
 
-require golang.org/x/crypto v0.24.0
+require (
+	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.24.0
+)
